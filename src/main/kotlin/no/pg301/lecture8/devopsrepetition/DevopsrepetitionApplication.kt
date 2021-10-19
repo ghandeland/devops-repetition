e2dcdac2d84a12bf70e7944f3e70eb3dae7918b7
ghandeland <gabriel.handeland@gmail.com>
2021-10-19 09:36:25 +0200

    initial commit

---
+++ src/main/kotlin/no/pg301/lecture8/devopsrepetition/DevopsrepetitionApplication.kt
@@ -0,0 +1,11 @@
+package no.pg301.lecture8.devopsrepetition
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevopsrepetitionApplication
+
+fun main(args: Array<String>) {
+	runApplication<DevopsrepetitionApplication>(*args)
+}
---
+++ src/test/kotlin/no/pg301/lecture8/devopsrepetition/DevopsrepetitionApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.pg301.lecture8.devopsrepetition
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevopsrepetitionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
